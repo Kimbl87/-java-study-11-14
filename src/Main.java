0140ab216082044a2df43a2d56192c67c37a9d25
Денис <ivanovds87@mail.ru>
2022-10-25 22:44:57 +0300

    Study 11 - fin

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String m = sc.next();
+        Integer n = sc.nextInt();
+        int resultCompare;
+        resultCompare = n.compareTo(Integer.valueOf(m));
+        if (resultCompare>0) {
+            System.out.println(n);
+            System.out.println(Double.valueOf(m));
+        }
+        else {
+            System.out.println(m);
+            System.out.println(Double.valueOf(n));
+        }
+
+
+
+    }
+}
+
+
