5fe202c73e2149f5995b1dfced416e896b5c100b
Денис <ivanovds87@mail.ru>
2022-10-25 23:11:25 +0300

    Study 12 - fin

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String m = sc.next();
-        Integer n = sc.nextInt();
-        int resultCompare;
-        resultCompare = n.compareTo(Integer.valueOf(m));
-        if (resultCompare>0) {
-            System.out.println(n);
-            System.out.println(Double.valueOf(m));
-        }
-        else {
-            System.out.println(m);
-            System.out.println(Double.valueOf(n));
-        }
+        final String  J = "I like Java!!!";
 
-
-
+//        1. Проверить, содержит ли строка подстроку “Java” (используйте contains()).
+        boolean javaCont = J.contains("Java");
+//        2. Проверить, начинается ли строка с подстроки “I like” (используйте startsWith()).
+        boolean javaStart = J.startsWith("I like");
+//        3. Проверить, заканчивается ли строка с подставки “!!!” (используйте endsWith()).
+        boolean javaEnd = J.endsWith("!!!");
+//        4. Если 3 предыдущих условия верны, выведите данную строку, преобразованную к верхнему регистру.
+        if (javaCont && javaStart && javaEnd) System.out.println(J.toUpperCase(Locale.ROOT));
+//        5. Замените все символы ‘a’ на ‘о’ и введите подстроку “Jovo” на экран (используйте substring()).
+        String jo = J.replace('a', 'o');
+        System.out.println(jo.substring(7,11));
     }
 }
 
