494ce01156e6da677401cb47b5526065ac8949ef
Денис <ivanovds87@mail.ru>
2022-10-25 23:13:11 +0300

    Study 12 - fin

--- src/Main_12.java
+++ src/Main_12.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 
 public class Main_12 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         final String  J = "I like Java!!!";
 
 //        1. Проверить, содержит ли строка подстроку “Java” (используйте contains()).
